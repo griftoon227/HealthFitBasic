db314e11fc81085e4112024d52b06f11c1a44140
Tyler DeMartino <tmdm98@hotmail.com>
2018-11-13 20:34:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tmdm9/healthfitbasic/activity_targets.java
@@ -0,0 +1,13 @@
+package com.example.tmdm9.healthfitbasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_targets extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_targets);
+    }
+}
